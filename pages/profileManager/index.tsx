a7d41830b67ecd114abca086f1156056ab94192c
anmol-dhiman <anmoldhiman7111@gmail.com>
2023-11-06 07:18:45 +0530

    profile manager update error

--- pages/profileManager/index.tsx
+++ pages/profileManager/index.tsx
@@ -7,15 +7,24 @@ import { TypedDataDomain } from 'viem';
 import { getAuthenticatedClient, getClient } from '../../utils/LensClient';
 import toast, { Toaster } from 'react-hot-toast';
 
+type Profile = {
+    createdAt: string,
+    signless: boolean,
+    handle: string | undefined,
+    id: string
+}
+
+
 const ProfileManager: NextPage = () => {
 
-    const [userProfiles, setProfiles] = useState<PaginatedResult<ProfileFragment>>()
+    const [userProfiles, setProfiles] = useState<Profile[]>([])
     const account = useAccount()
     const client = getClient()
+    const [loading, setLoading] = useState(false)
     const authFailure = () => toast.error("You rejected authentication from metamask!")
     const signatureFailure = () => toast.error("You rejected message signing from metamask!")
 
-    const toggleProjectManager = async (profile: ProfileFragment) => {
+    const toggleProjectManager = async (profile: Profile, index: number) => {
         try {
 
             const client = await getAuthenticatedClient(account.address!, profile.id)
@@ -52,8 +61,9 @@ const ProfileManager: NextPage = () => {
 
                 const success = () => toast.success(`Project Manager successfully ${profile.signless ? "removed" : "set"} for ID :${profile.id}`)
                 success()
-
-                await getProfiles()
+                const updatedProfile = userProfiles[index]
+                updatedProfile.signless = !profile.signless
+                setProfiles([...userProfiles, updatedProfile])
             } catch (e) {
                 signatureFailure()
             }
@@ -64,22 +74,39 @@ const ProfileManager: NextPage = () => {
 
 
     const getProfiles = async () => {
-
         const allOwnedProfiles = await client.profile.fetchAll({
             where: {
                 ownedBy: [account.address!],
             },
         })
+        const profiles: Profile[] = []
 
-        setProfiles(allOwnedProfiles)
+        allOwnedProfiles.items.forEach((item) => {
+            const id = item.id
+            const createdAt = item.createdAt
+            const handle = item.handle?.fullHandle
+            const signless = item.signless
+            profiles.push(
+                {
+                    id,
+                    handle,
+                    createdAt,
+                    signless
+                }
+            )
+        })
+        setProfiles(profiles)
     }
     useEffect(() => {
-        if (account.isConnected)
-            getProfiles()
+        const profiles = async () => {
+            if (account.isConnected) {
+                setLoading(true)
+                await getProfiles()
+                setLoading(false)
+            }
+        }
+        profiles()
     }, [account.isConnected])
-
-
-
 
 
     return (
@@ -91,17 +118,24 @@ const ProfileManager: NextPage = () => {
                 </div>
             }
             {
-                account.isConnected &&
+                loading &&
+                <div className=' font-semibold text-2xl text-center  '  >
+                    Fetching User Profiles....
+                </div>
+
+            }
+            {
+                account.isConnected && !loading &&
                 <div>
                     {
-                        userProfiles?.items.map((item) => {
+                        userProfiles.map((item, index) => {
                             return <>
                                 <div className='px-10 mb-6' key={item.id} >
                                     <div className='flex flex-row items-center   ' >
                                         <div className=' flex flex-row flex-1  items-center' >
                                             <div className='mr-8 flex-1 ' >
                                                 <p className=' info-header ' >Handle</p>
-                                                <p className='  info-text     ' >{item.handle?.fullHandle}</p>
+                                                <p className='  info-text     ' >{item.handle}</p>
                                             </div>
                                             <div className='mr-8 flex-1 ' >
                                                 <p className=' info-header ' >ID</p>
@@ -117,7 +151,7 @@ const ProfileManager: NextPage = () => {
                                                 <p className='  info-text     ' >{item.createdAt}</p>
                                             </div>
                                         </div>
-                                        <button className=' bg-[#0e76fd]  rounded-lg px-8 py-2 text-white font-bold  ' onClick={() => toggleProjectManager(item)}  >
+                                        <button className=' bg-[#0e76fd]  rounded-lg px-8 py-2 text-white font-bold  ' onClick={() => toggleProjectManager(item, index)}  >
                                             {
                                                 item.signless ? "Disable Project Manager" : "Enable Project Manager"
                                             }
